855d17137e1eb89948b951bc9ffb5eef987c50d5
xiangjianhuanxjh <1903371036@qq.com>
2024-01-16 08:42:13 +0800

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 8001,
+    open: true,
+    proxy: {
+      '/app-dev': {
+        target: 'http://localhost:8080/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/app-dev/, '')
+      }
+    }
+  }
 })
